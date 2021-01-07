d6c81a930c6a067da8cd805ce5910f652714f2c2
kariukirobert <robmainah@gmail.com>
2021-01-07 13:57:56 +0300

    sqlite

--- src/store.js
+++ src/store.js
@@ -2,27 +2,60 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import * as ApplicationSettings from "application-settings";
 
+const Sqlite = require("nativescript-sqlite");
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        firstname: "",
-        lastname: ""
+        database: null,
+        data: []
     },
     mutations: {
-        load(state) {
-            if (ApplicationSettings.getString("store")) {
-                this.replaceState(
-                    Object.assign(state, JSON.parse(ApplicationSettings.getString("store")))
-                );
+        init(state, data) {
+            state.database = data.database;
+        },
+        load(state, data) {
+            state.data = [];
+            for (var i = 0; i < data.data.length; i++) {
+                state.data.push({
+                    firstname: data.data[i][0],
+                    lastname: data.data[i][1]
+                });
             }
         },
         save(state, data) {
-            state.firstname = data.firstname;
-            state.lastname = data.lastname;
-        }
+            state.data.push({
+                firstname: data.data.firstname,
+                lastname: data.data.lastname
+            });
+        },
     },
     actions: {
-
+        init(context) {
+            (new Sqlite("my.db")).then(db => {
+                db.execSQL("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT)").then(id => {
+                    context.commit("init", { database: db });
+                }, error => {
+                    console.log("CREATE TABLE ERROR", error);
+                });
+            }, error => {
+                console.log("OPEN DB ERROR", error);
+            });
+        },
+        insert(context, data) {
+            context.state.database.execSQL("INSERT INTO people (firstname, lastname) VALUES (?, ?)", [data.firstname, data.lastname]).then(id => {
+                context.commit("save", { data: data });
+            }, error => {
+                console.log("INSERT ERROR", error);
+            });
+        },
+        query(context) {
+            context.state.database.all("SELECT firstname, lastname FROM people", []).then(result => {
+                context.commit("load", { data: result });
+            }, error => {
+                console.log("SELECT ERROR", error);
+            });
+        }
     }
 });
